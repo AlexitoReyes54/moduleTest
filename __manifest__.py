16ecdcd38a68ecf33805cc41703a645816b2ad6e
AlexitoReyes54 <alexandereliasr9@gmail.com>
2026-07-28 11:22:35 -0400

    manifest for installation

---
+++ __manifest__.py
@@ -0,0 +1,23 @@
+{
+    "name": "E-Commerce manager Watch",
+    "summary": "Mock store implementation ",
+    "description": """
+        E-Commerce Custom Extension
+    """,
+    "author": "Alexander Reyes",
+    "category": "Website/E-Commerce",
+    "version": "0.1",  # Update version based on your target Odoo instance
+    "depends": [
+        "base",
+        "crm",
+    ],
+    "data": [
+        # "security/ir.model.access.csv",
+        # "views/client_views.xml",
+        # "views/interaction_views.xml",
+    ],
+    "installable": True,
+    "application": True,
+    "auto_install": False,
+    "license": "LGPL-3",
+}
